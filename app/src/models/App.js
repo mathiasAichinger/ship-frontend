808beafaab9482b724d00aeef48f747fd1aadc02
Ronald Blüthl <ronald.bluethl@netxautomation.com>
2016-02-20 18:30:26 +0100

    ADD App.js model

---
+++ app/src/models/App.js
@@ -0,0 +1,7 @@
+function App (id, key, name, description, iconUrl) {
+    this.id = id;
+    this.key = key;
+    this.name = name;
+    this.description = description;
+    this.iconUrl = iconUrl;
+}
